414ed8d020319b8650387310ffb2cb7ae6898db4
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2022-10-26 14:40:12 -0500

    Agregamos las tarjetas totales de pokemon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//console.log(data);
+//import { example } from './data.js';
+//document.getElementsByName(bulbasaur)
+const pokemonName = data.pokemon
+const contenedor = document.getElementById("card")
 
-console.log(example, data);
+pokemonName.forEach((pokemon) => {
+    const tarjeta = `<div class="card"><p>${pokemon.name}</p><br><img src="${pokemon.img}"><br><p>Type: ${pokemon.type}</p><br><p>Evolution: ${pokemon.evolution['next-evolution:name']}`
+    contenedor.insertAdjacentHTML('beforebegin', tarjeta)
+});
+
+
+
+
+
+
+
+
+
+
+
+// const pokemonName = data.pokemon
+// pokemonName.forEach((pokemon) =>
+// console.log(pokemon)
+// );
+// console.log(data)
+// //})
