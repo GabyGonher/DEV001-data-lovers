42f5e2767363d8980d95a28918aabf3d6e8c0d86
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2022-11-02 12:44:04 -0600

    Borrador 1 filtrado, estilo en tarjetas, menu encabezado

--- src/main.js
+++ src/main.js
@@ -1,18 +1,48 @@
+
 import data from './data/pokemon/pokemon.js';
-//console.log(data);
-//import { example } from './data.js';
-//document.getElementsByName(bulbasaur)
-const pokemonName = data.pokemon
+import { filtraTipos } from './data.js';
+
+const cadaPokemon = data.pokemon
 const contenedor = document.getElementById("card")
 
-pokemonName.forEach((pokemon) => {
-    const tarjeta = `<div class="card"><p>${pokemon.name}</p><br><img src="${pokemon.img}"><br><p>Type: ${pokemon.type}</p><br><p>Evolution: ${pokemon.evolution['next-evolution:name']}`
-    contenedor.insertAdjacentHTML('beforebegin', tarjeta)
-});
 
-// const pokemonName = data.pokemon
-// pokemonName.forEach((pokemon) =>
-// console.log(pokemon)
-// );
-// console.log(data)
-// //})
+
+
+cadaPokemon.forEach((pokemon) => {
+
+    // const evoluciones = pokemon.evolution['next-evolution']
+    // console.log(evoluciones)
+
+    // if (evoluciones === ("next-evolution" ||  "prev-evolution")){
+    //     console.log(evoluciones)
+    // } else {
+    //     console.log ("no tiene evolución")
+    // }
+    // console.log(evoluciones)
+
+    const tarjeta =
+        `<div class="card">
+    <div class= "front">
+    <p class="name">${pokemon.name}</p>
+    <img src="${pokemon.img}">
+    <p class="pk_num">${pokemon.num}</p>
+    <p>Type: ${pokemon.type[0]}</p>
+    <p>Rarity: ${pokemon["pokemon-rarity"]}</p>
+    </div>`
+    // <div class="back">
+    // <p>Generation: ${pokemon.generation['name']}</p><br>
+    // <p>About: ${pokemon.about}</p>
+    // </div>
+    contenedor.insertAdjacentHTML('beforebegin', tarjeta)
+
+
+    const Tipos = document.getElementById("grass")
+    Tipos.addEventListener("click", function () {
+        document.getElementsByClassName('contenedor')[0].innerHTML = ""
+
+
+
+
+    })
+
+})
