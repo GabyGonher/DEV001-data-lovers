7601f2db1a58d457b758562a599ded43b7b0546d
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2022-11-16 13:36:04 -0600

    error en test vs  filtro funcionando

--- src/data.js
+++ src/data.js
@@ -1,15 +1,45 @@
-
-// import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
 const cadaPokemon = data.pokemon
 
 
-export const filtraTipos = (type) => {    
+
+
+// export const filtraTipos = (type,cadaPokemon) => {  
+//     const filterType= cadaPokemon.filter((element) => 
+//         element.type.includes(type)
+        
+//     );
+//     console.log(filterType);
+//     return filterType;
+// };
+
+export const filtraTipos = (type) => {
     // console.log(type)
     return cadaPokemon.filter((element) => {
         return element.type.includes(type);
     });
 }
 
-   
+
+   export const ordenarAz = (cadaPokemon) => {
+        let arrayOrdenar = [...cadaPokemon]
+        const pokemonAz = arrayOrdenar.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+              
+            }
+        });
+            return pokemonAz;
+        
+            
+    };
+// console.log(ordenarAz)
+
+
+// const filtraBuscador = (name) => {
+//     console.log(name)
+//     return cadaPokemon.filter((element) => {
+//         return element.name.includes(pikachu);
+//     });
+// }
