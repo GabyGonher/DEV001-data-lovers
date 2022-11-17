09151555706351f96c2dff6b324d852d03d44677
Caro-V <carolinavelasquez@MacBook-Air-de-Carolina.local>
2022-11-17 11:55:01 -0500

    sort, buscador

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ const cadaPokemon = data.pokemon
 // export const filtraTipos = (type,cadaPokemon) => {  
 //     const filterType= cadaPokemon.filter((element) => 
 //         element.type.includes(type)
-        
+
 //     );
 //     console.log(filterType);
 //     return filterType;
@@ -21,20 +21,38 @@ export const filtraTipos = (type) => {
     });
 }
 
+// export const ordenarNum = cadaPokemon.sort((a, b) => {
+//     return a.num - b.num
+// });console.log(ordenarNum)
 
-   export const ordenarAz = (cadaPokemon) => {
-        let arrayOrdenar = [...cadaPokemon]
-        const pokemonAz = arrayOrdenar.sort((a, b) => {
-            if (a.name < b.name) {
-                return -1;
-              
-            }
-        });
-            return pokemonAz;
-        
-            
-    };
-// console.log(ordenarAz)
+export const ordenarAZ = () => {
+    
+    return cadaPokemon.sort((a, b) => {
+    a.num - b.num
+    if (a.name < b.name) {
+        return -1;
+    } else { 1 }
+});
+};
+
+export const ordenarZA = () => {
+    
+    return cadaPokemon.sort((a, b) => {
+    a.num - b.num
+    if (b.name < a.name) {
+        return -1;
+    } else { 1 }
+});
+};
+
+export const filtraBuscador = (name) => {
+    console.log(name)
+    const buscador= cadaPokemon.filter((name) => {
+        name.target.matches("")
+    });
+    // console.log(buscador)
+    return buscador
+}
 
 
 // const filtraBuscador = (name) => {
