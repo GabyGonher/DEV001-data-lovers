de3992aedcaf2b60a2d85f981ba94950500c3322
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2022-11-18 14:41:47 -0600

    test funcionando, modal estadistica

---
+++ README POKÉMON.md
@@ -0,0 +1,72 @@
+# Data Lovers: Pokémon
+
+
+<!-- así como una introducción a la aplicación
+decisiones de diseño que tomaron.
+uso de tu aplicación web – funcionalidad
+Esta aplicación web, puede ser adaptada a cualquier estilo, requerimientos y compañía,  -->
+
+
+## Índice
+
+* [1. Introducción
+* [2. Diseño de la interfaz del usuario
+* [3. Objetivos
+  * [3.1 Historias de Usuario 
+* [4. Uso
+
+***
+
+## 1. Introducción
+En el presente documento se plasma el proceso de desarrollo del proyecto Data Lovers elaborado como parte de la curricula del bootcamp de Laboratoria - Cohort DEV001. A cargo de la dupla DL22: Carolina Velásquez Rodríguez y Gabriela González Herrera.
+
+
+## 2. Objetivos
+El objetivo de este proyecto es entender y procesar la información de un conjunto de datos, en este caso se trabajo con la data de Pokémon la cual contiene información detallada de 251 pokémones de las generaciones Jotho y Kanto. Siendo el objetivo principal la creación de una interfaz donde se pueda visualizar  un set de datos que se adecúe a lo que el usuario necesita.
+
+Para lograr los objetivos anteriores se llevo a cabo una encuesta para conocer las necesidades de los usuarios.
+
+(...Imagen de encuesta)
+
+
+
+Obteniendo los siguiente resultados
+(....Imagen de resultados de encuesta)
+
+
+HU1. Como usuario de PokémonGo quiero poder visualizar los pokémones con su imagen, nombre y generación para conocerlos y encontrarlos más fácilmente
+    -La UI sigue el modelo del prototipo de alta fidelidad
+La UI es responsive (telefonos y PC)
+Todos los pokemon aparecen en una misma vista, en forma de grilla (cuadricula) sin niun orden en particualar
+La vista no tiene paginacion, se hace scroll  desde el primer hasta el ultimo 
+Cada cuadricula contiene: nombre del pokemon e imagen
+
+HU2. Como usuario de Pokemon Go quiero poder visualizar a los pokemon por su tipo (water,grass,etc) para elegirlos mejor durante un combate
+
+HU3. Como usuario de Pokémon Go quiero poder buscar rápidamente los Pokemon por su nombre a través de un buscador y ordenador por abecedario integrado en la interfaz
+
+
+
+
+
+
+
+
+
+
+## 3. Desiciones de diseño
+
+
+
+
+
+
+
+
+
+
+## 4. Uso
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,62 +1,71 @@
 import data from './data/pokemon/pokemon.js';
-import { filtraTipos, ordenarAZ, ordenarZA, filtraBuscador } from './data.js';
+import { filtraTipos, ordenarAZ, ordenarZA, } from './data.js';
 
 
 
 const cadaPokemon = data.pokemon
 const contenedor = document.getElementById("cardList")
 const divParaModal = document.getElementById("divParaModal")
-
-
-const tarjetasPokemon = (arrPoke) => {
-    contenedor.innerHTML = '';
-    // console.log(arrPoke)
-    arrPoke.forEach((pokemon) => {
-
-        const tarjeta = document.createElement("div");
-        tarjeta.className = "card"
-        tarjeta.innerHTML =
-        `<div class= "front">
-            <p class="name">${pokemon.name}</p>
-            <p class="img" ><img src="${pokemon.img}"></p>
-            <p class="pk_num">${pokemon.num}</p>
-            <div><span class="name-card">Type</span><br> ${pokemon.type.map((type) => `<span class="typeClass ${type}">${type}</span>`).join(" / ")}</div>
-            <p><span class="name-card">Generation</span><br> ${pokemon["generation"].name}</p>
-            <button id="openModal" class="open" name="openModal">Learn more</button>
-        </div>`
-        contenedor.appendChild(tarjeta)
-
-
-        const modal = document.createElement("div");
-        modal.className = "modal-container"
-        modal.id = "modal-container"
-        modal.innerHTML =
+const crearModal = (pokemon) => {
+    const modal = document.createElement("div");
+    modal.className = "modal-container"
+    modal.id = "modal-container"
+    modal.innerHTML =
         `<div class= "modal">
             <p class="name-modal">${pokemon.name}</p>
             <p class="img"><img src="${pokemon.img}"></p>
             <p class="text-modal">${pokemon.about}</p>
             <div class="text-modal">Attack ${pokemon["special-attack"].map((ataque) =>
-                `<li class="ataqueClass ${ataque}"> ${ataque.name}</li>`).join(", ")}</div> 
+            `<li class="ataqueClass ${ataque}"> ${ataque.name}</li>`).join(", ")}</div> 
             <p class="text-modal">Height ${pokemon["size"].height}</p>
             <p class="text-modal">Weight ${pokemon["size"].height}</p>
             <button id="closeModal" class="close">X</button>
         </div>`
-        divParaModal.appendChild(modal)
+    divParaModal.innerHTML = '';
+    divParaModal.appendChild(modal)
+
+}
+
+const tarjetasPokemon = (arrPoke) => {
+    contenedor.innerHTML = '';
+    arrPoke.forEach((pokemon, index) => {
+
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "card"
+        tarjeta.innerHTML =
+            `<div class= "front">
+            <p class="name">${pokemon.name}</p>
+            <p class="img" ><img src="${pokemon.img}"></p>
+            <p class="pk_num">${pokemon.num}</p>
+            <div><span class="name-card">Type</span><br> ${pokemon.type.map((type) => `<span class="typeClass ${type}">${type}</span>`).join(" / ")}</div>
+            <p><span class="name-card">Generation</span><br> ${pokemon["generation"].name}</p>
+            <button id="${index}" class="open" name="openModal">Learn more</button>
+        </div>`
+        contenedor.appendChild(tarjeta)
+
+
+
 
         const open = document.querySelectorAll(".open")
-        const modal_container = document.getElementById("modal-container")
-        const close = document.getElementById("closeModal")
 
-        open.forEach(arrPoke => {
-            arrPoke.addEventListener("click", function eventoModal() {
+
+
+        open.forEach(btn => {
+            btn.addEventListener("click", function eventoModal(e) {
+                const pokeIndex = parseInt(e.target.id)
+                const selectedPoke = arrPoke[pokeIndex]
+                crearModal(selectedPoke)
+                const modal_container = document.getElementById("modal-container")
                 modal_container.classList.add("show");
                 // console.log(arrPoke)
+                const close = document.getElementById("closeModal")
+                close.addEventListener("click", () => {
+                    modal_container.classList.remove("show")
+                })
             })
         })
 
-        close.addEventListener("click", () => {
-            modal_container.classList.remove("show")
-        })
+
     })
 
 };
@@ -64,31 +73,134 @@ const tarjetasPokemon = (arrPoke) => {
 (tarjetasPokemon(cadaPokemon))
 
 
+
 const listaTipos = document.querySelectorAll('.menuDesplegable button')
 
 listaTipos.forEach((button) => {
+
     button.addEventListener("click", function eventos(e) {
-        tarjetasPokemon(filtraTipos(e.target.value))
+        contenedor.setAttribute("filter", e.target.value)
+        const resultado = filtraTipos(e.target.value, cadaPokemon)
+        tarjetasPokemon(resultado)
+        
+
     });
 
 });
 
 const buttonOrdenarAZ = document.getElementById('buttonOrdenarAZ')
 buttonOrdenarAZ.addEventListener("click", function () {
-    tarjetasPokemon(ordenarAZ())
+    if (contenedor.getAttribute("filter") == "none") {
+        tarjetasPokemon(ordenarAZ(cadaPokemon))
+    }
+    else {
+        const filtroElegido = contenedor.getAttribute("filter")
+        const resultado = filtraTipos(filtroElegido, cadaPokemon)
+        tarjetasPokemon(ordenarAZ(resultado))
+
+    }
 
 });
 
 const buttonOrdenarZA = document.getElementById('buttonOrdenarZA')
 buttonOrdenarZA.addEventListener("click", function () {
-    tarjetasPokemon(ordenarZA())
+    if (contenedor.getAttribute("filter") == "none") {
+        tarjetasPokemon(ordenarZA(cadaPokemon))
+    }
+    else {
+        const filtroElegido = contenedor.getAttribute("filter")
+        const resultado = filtraTipos(filtroElegido, cadaPokemon)
+        tarjetasPokemon(ordenarZA(resultado))
+
+    }
 
 });
 
-const inputBuscar= document.getElementById("inputBuscar")
-inputBuscar.addEventListener("keyup", () => {
-    const valor = inputBuscar.value
-    filtraBuscador (valor)
-        }
-    );
 
+const btnEstadistica = document.getElementById("btnEstadistica")
+const divOpaco = document.getElementById("divOpaco")
+const closeEstadistica = document.getElementById("closeEstadistica")
+btnEstadistica.addEventListener("click", () => {
+    divOpaco.classList.add("show");
+})
+closeEstadistica.addEventListener("click", () => {
+    divOpaco.classList.remove("show");
+
+})
+
+// Cantidad total de Pokemon
+//Del array general sacar uno por cada tipo 
+//Contar la longitud de cada uno  
+
+
+
+
+
+
+
+// //AQUI VA EL METODO REDUCE
+
+// export const reductionXType = (objectData) => {
+//     const reducePerType = objectData.reduce((obj, element) => {
+//         if (obj[element] != undefined) {
+//             obj[element] += 1;
+//         } else {
+//             obj[element] = 1;
+//         }
+
+//         return obj
+//     }, {})
+//     return reducePerType
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputBuscar= document.getElementById("inputBuscar")
+// inputBuscar.addEventListener("keyup", () => {
+//     const valor = inputBuscar.value
+//     filtraBuscador (valor)
+//         }
+//     );
+
+// const inputBuscar = document.getElementById("inputBuscar")
+// inputBuscar.addEventListener("keyup", () => {
+//     console.log()
+//     const valor = inputBuscar.value
+//     filtraBuscador(valor, cadaPokemon)
+// }
+// );
+
+
+// //*aqui va el find*/
+
+// export const findById = (id, pokemonData) => {
+//     const findbyidPokemon = pokemonData.find(
+//         (pokemon) => pokemon.num == id);
+//     //console.log(findbyidPokemon);
+//     return findbyidPokemon;
+// };
