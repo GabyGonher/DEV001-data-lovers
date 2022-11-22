a53c0111020e2b197352f0c148dde7ea871d2502
Caro-V <carolinavelasquez@MacBook-Air-de-Carolina.local>
2022-11-22 14:20:05 -0500

    Últimos cambios en css

--- src/data.js
+++ src/data.js
@@ -1,48 +1,49 @@
-export const filtraTipos = (type, arrPokemon) => {  
-    const filterType = arrPokemon.filter((element) => 
+//Función para filtrar por tipos
+export const filtraTipos = (type, arrPokemon) => {
+    const filterType = arrPokemon.filter((element) =>
         element.type.includes(type)
     )
-
-    // console.log(filterType)
-    return filterType;   
+    return filterType;
 };
 
-
+//Función para ordenar de la A a la Z
 export const ordenarAZ = (arrPokemon) => {
     return arrPokemon.sort((a, b) => {
-    a.num - b.num
-    if (a.name < b.name) {
-        return -1;
-    } else { 1 }
-});
+        a.num - b.num
+        if (a.name < b.name) {
+            return -1;
+        } else { 1 }
+    });
 };
 
+//Función para ordenar de la Z a la A
 export const ordenarZA = (arrPokemon) => {
     return arrPokemon.sort((a, b) => {
-    a.num - b.num
-    if (b.name < a.name) {
-        return -1;
-    } else { 1 }
-});
+        a.num - b.num
+        if (b.name < a.name) {
+            return -1;
+        } else { 1 }
+    });
 };
 
+//Función para el buscador
 export const filtraBuscador = (name, arrPokemon) => {
     const buscador = arrPokemon.filter(pokemon => pokemon.name.includes(name));
     return buscador
 }
 
+//Función reduce para mostrar estadística
 export const reduceType = (arrPokemon) => {
-const tiposFlat = arrPokemon.map((pokemon) => pokemon.type).flat()
-const contarTipos = tiposFlat.reduce(function(obj, tipos){
-    if (!obj[tipos]){
-        obj[tipos]=1;
-    } else {
-        obj[tipos]++
-    }
-    
-    return obj;
-}, {});
-// console.log(contarTipos)
-return contarTipos
+    const tiposFlat = arrPokemon.map((pokemon) => pokemon.type).flat()
+    const contarTipos = tiposFlat.reduce(function (obj, tipos) {
+        if (!obj[tipos]) {
+            obj[tipos] = 1;
+        } else {
+            obj[tipos]++
+        }
+
+        return obj;
+    }, {});
+    return contarTipos
 }
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { filtraTipos, ordenarAZ, ordenarZA, filtraBuscador, reduceType } from '.
 const cadaPokemon = data.pokemon
 const contenedor = document.getElementById("cardList")
 const divParaModal = document.getElementById("divParaModal")
+
+//Creamos modal para cada Pokemon
 const crearModal = (pokemon) => {
     const modal = document.createElement("div");
     modal.className = "modal-container"
@@ -12,25 +14,27 @@ const crearModal = (pokemon) => {
     modal.innerHTML =
 
         `  <div class= "modal">
+            <div><button id="closeModal" class="close">X</button></div>
             <p class="name-modal">${pokemon.name}</p>
             <p class="img"><img src="${pokemon.img}"></p>
             <p class="text-modal">${pokemon.about}</p><br>
             <div class= "modalInfo">
-            <div class="text-modal"> <span class="name-card">Attack</span> ${pokemon["special-attack"].map((ataque) =>
-            `<li class="ataqueClass ${ataque}">${ataque.name}</li>`).join("")}</div>
-            <div class="text-modal"> <span class="name-card">Weaknesses</span> ${pokemon["weaknesses"].map((debilidad) =>
-                `<li class="ataqueClass ${debilidad}">${debilidad}</li>`).join("")}</div> 
-            <div class="text-modal"> <span class="name-card">Resistant</span> ${pokemon["resistant"].map((resistencia) =>
-                    `<li class="ataqueClass ${resistencia}">${resistencia}</li>`).join("")}</div>  
-            <p class="text-modal"> <span class="name-card">Height</span> <br> ${pokemon["size"].height}</p> 
+            <p class="text-modal"> <span class="name-card">Height</span><br> ${pokemon["size"].height}</p>
             <p class="text-modal"> <span class="name-card"> Weight</span><br> ${pokemon["size"].height}</p>
+            <div class="text-modal"> <span class="name-card">Attack</span><br> ${pokemon["special-attack"].map((ataque) =>
+            `<li class="ataqueClass ${ataque}">${ataque.name}</li>`).join("")}</div>
+            <div class="text-modal"> <span class="name-card">Weaknesses</span><br> ${pokemon["weaknesses"].map((debilidad) =>
+                `<li class="ataqueClass ${debilidad}">${debilidad}</li>`).join("")}</div> 
+            <div class="text-modal"> <span class="name-card">Resistant</span><br> ${pokemon["resistant"].map((resistencia) =>
+                `<li class="ataqueClass ${resistencia}">${resistencia}</li>`).join("")}</div> 
             </div>
-           <button id="closeModal" class="close">X</button>
+            
         </div>`
     divParaModal.innerHTML = '';
     divParaModal.appendChild(modal)
 }
 
+//Generamos tarjetas para cada Pokemon
 const tarjetasPokemon = (arrPoke) => {
     contenedor.innerHTML = '';
     arrPoke.forEach((pokemon, index) => {
@@ -43,7 +47,7 @@ const tarjetasPokemon = (arrPoke) => {
             <p class="img" ><img src="${pokemon.img}"></p>
             <p class="pk_num">${pokemon.num}</p>
             <div><span class="name-card">Type</span><br> ${pokemon.type.map((type) =>
-                `<span class="typeClass ${type}">${type}</span>`).join(" / ")}</div>
+                `<span class="ataqueClass ${type}">${type}</span>`).join(" / ")}</div>
             <p><span class="name-card">Generation</span><br> ${pokemon["generation"].name}</p>
             <button id="${index}" class="open" name="openModal">Learn more</button>
         </div>`
@@ -53,7 +57,7 @@ const tarjetasPokemon = (arrPoke) => {
         const open = document.querySelectorAll(".open")
 
 
-
+//Función para que al abrir el modal se muestre el pokemon que corresponde
         open.forEach(btn => {
             btn.addEventListener("click", function eventoModal(e) {
                 const pokeIndex = parseInt(e.target.id)
@@ -73,10 +77,9 @@ const tarjetasPokemon = (arrPoke) => {
 (tarjetasPokemon(cadaPokemon))
 
 
-
+//Función que muestra el array por tipos según lo seleccionado por el usuario
 const listaTipos = document.querySelectorAll('.menuDesplegable button')
 listaTipos.forEach((button) => {
-
     button.addEventListener("click", function eventos(e) {
         contenedor.setAttribute("filter", e.target.value)
         const resultado = filtraTipos(e.target.value, cadaPokemon)
@@ -84,6 +87,7 @@ listaTipos.forEach((button) => {
     });
 });
 
+//Función que ordene de la A a la Z
 const buttonOrdenarAZ = document.getElementById('buttonOrdenarAZ')
 buttonOrdenarAZ.addEventListener("click", function () {
     if (contenedor.getAttribute("filter") == "none") {
@@ -96,6 +100,7 @@ buttonOrdenarAZ.addEventListener("click", function () {
     }
 });
 
+//Función que ordena de la Z a la A
 const buttonOrdenarZA = document.getElementById('buttonOrdenarZA')
 buttonOrdenarZA.addEventListener("click", function () {
     if (contenedor.getAttribute("filter") == "none") {
@@ -108,7 +113,15 @@ buttonOrdenarZA.addEventListener("click", function () {
     }
 });
 
+//Función que muestra el pokemon según el input del buscador
+const inputBuscar = document.getElementById("inputBuscar")
+inputBuscar.addEventListener("keyup", () => {
+    const search = filtraBuscador(inputBuscar.value, cadaPokemon);
+    // console.log(search);
+    tarjetasPokemon(search)
+})
 
+//Creamos el modal para estadística
 const btnEstadistica = document.getElementById("btnEstadistica")
 const divOpaco = document.getElementById("divOpaco")
 const closeEstadistica = document.getElementById("closeEstadistica")
@@ -119,30 +132,27 @@ closeEstadistica.addEventListener("click", () => {
     divOpaco.classList.remove("show");
 })
 
-const inputBuscar = document.getElementById("inputBuscar")
-inputBuscar.addEventListener("keyup", () => {
-    const search = filtraBuscador(inputBuscar.value, cadaPokemon);
-    // console.log(search);
-    tarjetasPokemon(search)
-})
 
+//Se muestra el resultado de la estadística por tipos
 document.getElementById("conteoTipos").innerHTML =
-    `<p>Grass ${reduceType(cadaPokemon).grass}</p>
-<p>Fire ${reduceType(cadaPokemon).fire}</p>
-<p>Water ${reduceType(cadaPokemon).water}</p>
-<p>Electric ${reduceType(cadaPokemon).electric}</p>
-<p>Flying ${reduceType(cadaPokemon).flying}</p>
-<p>Normal ${reduceType(cadaPokemon).normal}</p>
-<p>Poison ${reduceType(cadaPokemon).poison}</p>
-<p>Ice ${reduceType(cadaPokemon).ice}</p>
-<p>Fighting ${reduceType(cadaPokemon).fighting}</p>
-<p>Ground ${reduceType(cadaPokemon).ground}</p>
-<p>Psychic ${reduceType(cadaPokemon).psychic}</p>
-<p>Bug ${reduceType(cadaPokemon).bug}</p>
-<p>Rock ${reduceType(cadaPokemon).rock}</p>
-<p>Ghost ${reduceType(cadaPokemon).ghost}</p>
-<p>Dark ${reduceType(cadaPokemon).dark}</p>
-<p>Dragon ${reduceType(cadaPokemon).dragon}</p>
-<p>Steel ${reduceType(cadaPokemon).steel}</p>
-<p>Fairy ${reduceType(cadaPokemon).fairy}</p>
-`
+    `<p class="statTitle">Number of Pokémon per type</p>
+    <div class="modalInfoEstats name-card">
+        <p class="open">Grass<br> ${reduceType(cadaPokemon).grass}</p>
+        <p class="open">Fire<br> ${reduceType(cadaPokemon).fire}</p>
+        <p class="open">Water<br> ${reduceType(cadaPokemon).water}</p>
+        <p class="open">Electric<br> ${reduceType(cadaPokemon).electric}</p>
+        <p class="open">Flying<br> ${reduceType(cadaPokemon).flying}</p>
+        <p class="open">Normal<br> ${reduceType(cadaPokemon).normal}</p>
+        <p class="open">Poison<br> ${reduceType(cadaPokemon).poison}</p>
+        <p class="open">Ice<br> ${reduceType(cadaPokemon).ice}</p>
+        <p class="open">Fighting<br> ${reduceType(cadaPokemon).fighting}</p>
+        <p class="open">Ground<br> ${reduceType(cadaPokemon).ground}</p>
+        <p class="open">Psychic<br> ${reduceType(cadaPokemon).psychic}</p>
+        <p class="open">Bug<br> ${reduceType(cadaPokemon).bug}</p>
+        <p class="open">Rock<br> ${reduceType(cadaPokemon).rock}</p>
+        <p class="open">Ghost<br> ${reduceType(cadaPokemon).ghost}</p>
+        <p class="open">Dark<br> ${reduceType(cadaPokemon).dark}</p>
+        <p class="open">Dragon<br> ${reduceType(cadaPokemon).dragon}</p>
+        <p class="open">Steel<br> ${reduceType(cadaPokemon).steel}</p>
+        <p class="open">Fairy<br> ${reduceType(cadaPokemon).fairy}</p>
+    </div>`
